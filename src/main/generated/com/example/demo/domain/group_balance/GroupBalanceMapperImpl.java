be0fb2c4f80ef7d73061c705c63a2b1e3ac8e0fc
hkedik <hanno.kedik@gmail.com>
2022-05-06 14:51:32 +0300

    commit

--- src/main/generated/com/example/demo/domain/group_balance/GroupBalanceMapperImpl.java
+++ src/main/generated/com/example/demo/domain/group_balance/GroupBalanceMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-06T12:00:25+0300",
+    date = "2022-05-06T14:50:26+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.3 (Amazon.com Inc.)"
 )
 @Component
